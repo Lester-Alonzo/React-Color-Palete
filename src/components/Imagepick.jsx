4498be3b460a39a21092e10db692136dacde84b0
Lester-Alonzo <lestergeo96@gmail.com>
2022-06-22 16:15:55 -0600

    [FIX]:cursor pointer on image selector

--- src/components/Imagepick.jsx
+++ src/components/Imagepick.jsx
@@ -13,7 +13,7 @@ export default function Imagepick() {
   }
   return (
     <div style={{display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column", gap:"0.4rem", marginTop:"1rem"}}>
-      <label htmlFor="img" style={{textAlign:'center', backgroundColor:"rgba(0,0,0, 0.5)", backdropFilter:"blur(5px)", padding:"0.3rem", color:"white", borderRadius:"11px", boxShadow:"1px 1px 11px rgba(0,0,0, 0.5)"}}>Seleccione una Image:</label>
+      <label htmlFor="img" style={{textAlign:'center', backgroundColor:"rgba(0,0,0, 0.5)", backdropFilter:"blur(5px)", padding:"0.3rem", color:"white", borderRadius:"11px", boxShadow:"1px 1px 11px rgba(0,0,0, 0.5)", cursor:"pointer"}}>Seleccione una Image:</label>
     {rl !== ''? <button onClick={handleClick}>x</button> : null}
       <input type="file" id="img" hidden onChange={handleChange}/>
       <img src={rl} style={{maxWidth:"100%", width:"60%", margin:"0 auto", boxShadow:"1px 1px 11px rgba(0,0,0, 0.5)"}}/>
